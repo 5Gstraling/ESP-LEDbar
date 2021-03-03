e3d06a4a9e61b717ab2b5337bc49b991b7380441
lisadv <78847178+lisadv@users.noreply.github.com>
2021-03-03 10:29:43 +0100

    Update main.cpp
    aanpassing naam client

--- src/main.cpp
+++ src/main.cpp
@@ -168,7 +168,7 @@ void reconnect() {
   while (!client.connected()) { // loop until we're reconnected
     Serial.print("Attempting MQTT connection...");
     // attempt to connect
-    if (client.connect("ESP8266Client")) {
+    if (client.connect("ESP_LEDbar")) {
       Serial.println("connected");
       client.subscribe("espMorse");
       client.subscribe("espOntsmet");
